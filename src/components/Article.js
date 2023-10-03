d97fc11a20cc8af837fc41703003654f41df6e90
Meet Rajput <rajputmeet10@gmail.com>
2023-10-03 11:43:42 +0530

    one more issue resolved

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,7 @@
 export default function Article(props){
     return(
         <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-4">
-             <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a>{props.postname}</a></h2>
+             <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.postname}</h2>
               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{props.post}</p>
               <div className="flex justify-between items-center">
                   <div className="flex items-center space-x-4">
